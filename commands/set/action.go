483d65fa2e24509f39bf5523c5fcd3dc676114bd
Matteo Baiguini SBT <mbaiguini@swissblock.net>
2020-01-07 16:33:53 +0100

    Completed set-local command

--- commands/set/action.go
+++ commands/set/action.go
@@ -5,19 +5,16 @@ import (
 	"path/filepath"
 
 	"bygui86/konf/commons"
-	"bygui86/konf/kubeconfig"
 	"bygui86/konf/logger"
 	"bygui86/konf/utils"
 
 	"github.com/urfave/cli"
 )
 
+// TODO: try to execute "source ./set-local-script.sh"
 func setLocal(ctx *cli.Context) error {
-	logger.Logger.Info("")
 	logger.Logger.Debug("🐛 Executing SET-LOCAL command")
 	logger.Logger.Debug("")
-
-	logger.Logger.Warn("⚠️  Work in progress")
 
 	logger.Logger.Debug("🐛 Get single Kubernetes configurations files path")
 	singleConfigsPath := ctx.String(commons.SingleConfigsFlagName)
@@ -29,7 +26,7 @@ func setLocal(ctx *cli.Context) error {
 			fmt.Sprintf("❌ Error checking existence of Kubernetes configurations files path '%s': %s", checkFolderErr.Error(), singleConfigsPath),
 			31)
 	}
-	logger.SugaredLogger.Infof("📚 Single Kubernetes configurations files path: '%s'", singleConfigsPath)
+	logger.SugaredLogger.Debugf("📚 Single Kubernetes configurations files path: '%s'", singleConfigsPath)
 
 	logger.Logger.Debug("🐛 Get selected Kubernetes context")
 	args := ctx.Args()
@@ -40,7 +37,7 @@ func setLocal(ctx *cli.Context) error {
 	}
 	context := args[0]
 
-	logger.SugaredLogger.Debugf("🐛 Check existence of single Kubernetes configurations files path '%s'", singleConfigsPath)
+	logger.SugaredLogger.Debugf("🐛 Check existence of single Kubernetes configurations file for context '%s'", context)
 	localKubeConfig := filepath.Join(singleConfigsPath, context)
 	checkFileErr := utils.CheckIfFileExist(localKubeConfig)
 	if checkFileErr != nil {
@@ -48,19 +45,9 @@ func setLocal(ctx *cli.Context) error {
 			fmt.Sprintf("❌ Error checking existence of Kubernetes context '%s': %s", checkFileErr.Error(), localKubeConfig),
 			33)
 	}
-	logger.SugaredLogger.Infof("🧩 Selected Kubernetes context: '%s'", context)
+	logger.SugaredLogger.Debugf("🧩 Selected Kubernetes context: '%s'", context)
 
-	logger.Logger.Debug("🐛 Set local Kubernetes context")
-	setErr := kubeconfig.SetKubeConfigEnvVar(localKubeConfig)
-	if setErr != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("❌ Error setting local Kubernetes context (env-var KUBECONFIG): %s", setErr.Error()),
-			34)
-	}
-	// logger.SugaredLogger.Infof("⚙️  Local Kubernetes context: '%s'", localKubeConfig)
-	logger.SugaredLogger.Infof("⚙️  Local Kubernetes context: '%s'", kubeconfig.GetKubeConfigEnvVar())
-
-	logger.Logger.Info("")
+	logger.Logger.Info(fmt.Sprintf("export %s='%s'", commons.KubeConfigEnvVar, localKubeConfig))
 	return nil
 }
 
